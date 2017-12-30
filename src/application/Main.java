85e054bf5fa2f9c4a4b900279839c009061323b7
Caleb Cook <calebcook@u.boisestate.edu>
2017-12-29 17:11:54 -0700

    Added two words

--- src/application/Main.java
+++ src/application/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 	        
 			Scene scene = new Scene(root,900,600);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setTitle("Quiz Bowl Auto Score");
+			primaryStage.setTitle("Quiz Bowl Auto Score Was Changed");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
